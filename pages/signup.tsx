e971b4c9f9cb2e939d4da2da9648d6d8d2f68df1
IconicDexter <kennymwendwa67@gmail.com>
2023-04-03 16:08:08 +0300

    commit

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import Image from "next/image"
 import Link from "next/link"
 import Img from "@/public/image.jpg"
@@ -5,7 +6,12 @@ import Img from "@/public/image.jpg"
 export default function Signup() {
   return (
     <>
-      {/* Head */}
+      <Head>
+        <title>Sign In</title>
+        <meta name="description" content="Sign In page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a
           href="#"
