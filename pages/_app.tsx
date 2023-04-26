1c0a2abb35687af5c11f5cf8ddabc9479152510a
IconicDexter <kennymwendwa67@gmail.com>
2023-04-26 19:45:20 +0300

    Added poppins font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,24 @@
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400"],
+  subsets: ["latin"],
+});
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </div>
+    <main className={poppins.className}>
+      <div className="bg-gray-100 min-h-screen">
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </div>
+    </main>
   );
 }
