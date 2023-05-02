a4bcbd83e14fc85c7211d3e163564fdcd80c6702
IconicDexter <kennymwendwa67@gmail.com>
2023-05-02 07:57:46 +0300

    commit

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,4 +1,13 @@
-export default function profile() {
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+export default function Profile() {
+  const { data: session } = useSession();
+  const router = useRouter();
+  if (!session) {
+    router.replace("/signin");
+    return null;
+  }
   return (
     <>
       <div>profile</div>
