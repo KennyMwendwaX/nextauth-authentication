0c1b647f44cee4c7f6ae61d136596238b20569da
IconicDexter <kennymwendwa67@gmail.com>
2023-05-06 12:23:33 +0300

    commit

--- pages/modal.tsx
+++ pages/modal.tsx
@@ -24,8 +24,8 @@ export default function Modal() {
             </button>
             <section className="bg-gray-50 dark:bg-gray-900">
               <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
-                  <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md sm:p-8">
+                  <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                     Change Password
                   </h2>
                   <form
@@ -34,14 +34,14 @@ export default function Modal() {
                     <div>
                       <label
                         htmlFor="email"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        className="block mb-2 text-sm font-medium text-gray-900">
                         Your email
                       </label>
                       <input
                         type="email"
                         name="email"
                         id="email"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                         placeholder="name@company.com"
                         required
                       />
@@ -49,7 +49,7 @@ export default function Modal() {
                     <div>
                       <label
                         htmlFor="password"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        className="block mb-2 text-sm font-medium text-gray-900">
                         New Password
                       </label>
                       <input
@@ -57,14 +57,14 @@ export default function Modal() {
                         name="password"
                         id="password"
                         placeholder="••••••••"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                         required
                       />
                     </div>
                     <div>
                       <label
                         htmlFor="confirm-password"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        className="block mb-2 text-sm font-medium text-gray-900">
                         Confirm password
                       </label>
                       <input
@@ -72,7 +72,7 @@ export default function Modal() {
                         name="confirm-password"
                         id="confirm-password"
                         placeholder="••••••••"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                         required
                       />
                     </div>
@@ -82,7 +82,7 @@ export default function Modal() {
                           id="newsletter"
                           aria-describedby="newsletter"
                           type="checkbox"
-                          className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                          className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
                           required
                         />
                       </div>
@@ -92,7 +92,7 @@ export default function Modal() {
                           className="font-light text-gray-500 dark:text-gray-300">
                           I accept the{" "}
                           <a
-                            className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                            className="font-medium text-primary-600 hover:underline"
                             href="#">
                             Terms and Conditions
                           </a>
@@ -101,7 +101,7 @@ export default function Modal() {
                     </div>
                     <button
                       type="submit"
-                      className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                      className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                       Reset password
                     </button>
                   </form>
