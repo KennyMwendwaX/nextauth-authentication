eaba3142b499a38717568b7f5bb72cc0b2acad14
IconicDexter <kennymwendwa67@gmail.com>
2023-09-09 17:17:02 +0300

    Add the nextauth route handler

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-gray-800 min-h-screen">
       {/* {status === "authenticated" ? ( */}
       <div className="pt-16">
         {/* Welcome {session?.user?.name}, {session?.user?.email} */}
         <button
           onClick={() => signOut()}
-          className="ml-2 rounded-lg bg-gray-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-400">
+          className="ml-2 rounded-lg bg-blue-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-400">
           Sign Out
         </button>
       </div>
@@ -21,12 +21,12 @@ export default function Home() {
           You are not signed in{" "}
           <Link
             href="/signin"
-            className="ml-2 rounded-lg bg-gray-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-400">
+            className="ml-2 rounded-lg bg-blue-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-400">
             Sign In
           </Link>
         </div>
       </div>
       {/* )} */}
-    </>
+    </div>
   );
 }
