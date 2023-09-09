bc314db52e2fd0bcece6b2cf8b4c486a75d7b596
IconicDexter <kennymwendwa67@gmail.com>
2023-09-09 17:43:22 +0300

    Integrated the GoogleProvider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,14 @@
+import type { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
