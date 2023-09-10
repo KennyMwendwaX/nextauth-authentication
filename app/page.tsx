dd55857b683dd58f8429378a4ff2800cecdadfba
IconicDexter <kennymwendwa67@gmail.com>
2023-09-10 12:28:26 +0300

    Created new fie for authOptions

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,33 @@
 "use client";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Home() {
+  const { data: session, status } = useSession();
+
   return (
     <div className="bg-gray-800 min-h-screen">
-      {/* {status === "authenticated" ? ( */}
-      <div className="pt-16">
-        {/* Welcome {session?.user?.name}, {session?.user?.email} */}
-        <button
-          onClick={() => signOut()}
-          className="ml-2 rounded-lg bg-blue-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-400">
-          Sign Out
-        </button>
-      </div>
-      {/* ) : ( */}
-      <div className="pt-16">
-        <div>
-          You are not signed in{" "}
-          <Link
-            href="/signin"
+      {status === "authenticated" ? (
+        <div className="pt-16">
+          Welcome {session?.user?.name}, {session?.user?.email}
+          <button
+            onClick={() => signOut()}
             className="ml-2 rounded-lg bg-blue-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-400">
-            Sign In
-          </Link>
+            Sign Out
+          </button>
         </div>
-      </div>
-      {/* )} */}
+      ) : (
+        <div className="pt-16">
+          <div>
+            You are not signed in{" "}
+            <Link
+              href="/signin"
+              className="ml-2 rounded-lg bg-blue-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-400">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
