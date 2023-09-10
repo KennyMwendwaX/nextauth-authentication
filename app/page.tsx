cd3f96794fe9c79746d85b28e8435966e7f7c0be
IconicDexter <kennymwendwa67@gmail.com>
2023-09-10 15:53:46 +0300

    Add profile image domains in next.config.js

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <div className="bg-gray-800 min-h-screen">
       {status === "authenticated" ? (
-        <div className="pt-16">
-          Welcome {session?.user?.name}, {session?.user?.email}
+        <div className="pt-20">
+          Welcome Name: {session?.user?.name}, Email: {session?.user?.email}
           <button
             onClick={() => signOut()}
             className="ml-2 rounded-lg bg-blue-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-400">
@@ -17,7 +17,7 @@ export default function Home() {
           </button>
         </div>
       ) : (
-        <div className="pt-16">
+        <div className="pt-20">
           <div>
             You are not signed in{" "}
             <Link
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,21 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
-import { FaUserCog } from "react-icons/fa";
-import { HiOutlineLogout } from "react-icons/hi";
-import type { Session } from "next-auth";
-import { signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { useSession } from "next-auth/react";
 
 export default function Navbar() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { data: session, status } = useSession();
+  const [profileImage, setProfileImage] = useState<string | null>(null);
 
-  const { data: session } = useSession();
+  useEffect(() => {
+    if (status === "authenticated") {
+      const userImage = session?.user?.image as string;
+      setProfileImage(userImage);
+    }
+  }, [session, status]);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
   return (
     <>
       <nav className="fixed bg-gray-900 px-2 sm:px-4 py-3 w-full z-20 top-0 left-0">
@@ -24,61 +25,27 @@ export default function Navbar() {
               Iconic
             </span>
           </Link>
-          {!session ? (
-            <></>
-          ) : (
-            <div className="flex items-center md:order-2">
-              <button
-                type="button"
-                className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300"
-                id="user-menu-button"
-                aria-expanded={isMenuOpen}
-                onClick={toggleMenu}
-                data-dropdown-toggle="user-dropdown"
-                data-dropdown-placement="bottom">
-                <span className="sr-only">Open user menu</span>
+          {status === "authenticated" ? (
+            <div className="ml-5 mr-3 text-sm cursor-pointer md:mr-0">
+              <span className="sr-only">Profile image</span>
+              {profileImage !== null ? (
                 <Image
                   className="rounded-full"
+                  src={profileImage}
                   width={32}
                   height={32}
-                  src="/profile-picture-3.jpg"
-                  alt="user photo"
+                  alt="profile-image"
                 />
-              </button>
-              {/* Dropdown menu */}
-              <div
-                className={`z-50 ${
-                  isMenuOpen ? "" : "hidden"
-                } fixed top-8 right-6 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow`}
-                id="user-dropdown">
-                <div className="px-4 py-3">
-                  <span className="block text-md font-semibold  text-gray-900">
-                    {/* {session.user?.name} */}
-                  </span>
-                  <span className="block text-sm font-light text-gray-500 truncate">
-                    {/* {session.user?.email} */}
-                  </span>
-                </div>
-                <ul className="py-2" aria-labelledby="user-menu-button">
-                  <li>
-                    <Link
-                      href="/profile"
-                      className="inline-flex w-full items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                      <FaUserCog size={18} />
-                      &nbsp; Profile
-                    </Link>
-                  </li>
-                  <li>
-                    <button
-                      onClick={() => signOut()}
-                      className="inline-flex w-full items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                      <HiOutlineLogout size={18} />
-                      &nbsp; Sign out
-                    </button>
-                  </li>
-                </ul>
-              </div>
+              ) : (
+                <FaUserCircle className="text-gray-400" size={32} />
+              )}
             </div>
+          ) : (
+            <Link
+              href="/signin"
+              className="ml-5 mr-3 rounded-lg bg-blue-800 hover:bg-blue-700 px-5 py-2 text-white">
+              Login
+            </Link>
           )}
         </div>
       </nav>
