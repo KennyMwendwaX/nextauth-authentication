b076d8cc01543ef82439bf71ddec1fcc1d0f33e4
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-25 08:52:51 +0300

    Added Logo to signin page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import Img from "@/public/image.jpg";
+import Logo from "@/public/logo-xs.png";
 import {
   Form,
   FormControl,
@@ -78,8 +78,7 @@ export default function Signin() {
         <a
           href="#"
           className="p-4 flex items-center text-2xl font-semibold text-black">
-          <Image className="mr-2 h-8 w-8" src={Img} alt="logo" />
-          Iconic
+          <Image width={40} height={40} src={Logo} alt="logo" />
         </a>
         <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0">
           <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
