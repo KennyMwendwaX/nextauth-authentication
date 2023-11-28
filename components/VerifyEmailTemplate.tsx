92b80141b8b9e39aca5c2c594a6c14835fa2d7cb
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-28 11:47:45 +0300

    Created verificationEmailTemplate component using react-email library

--- components/VerifyEmailTemplate.tsx
+++ components/VerifyEmailTemplate.tsx
@@ -1,7 +1,104 @@
-export default function VerifyEmailTemplate() {
-  return (
-    <>
-      <div>VerifyEmailTemplate</div>
-    </>
-  );
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+
+interface VerifyEmailProps {
+  verificationCode?: string;
 }
+
+export const VerifyEmail = ({
+  verificationCode = "DJZ-TLX",
+}: VerifyEmailProps) => (
+  <Html>
+    <Head />
+    <Preview>Verify your email address</Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Section style={logoContainer}>
+          <Img
+            src={`/public/logo-xs.png`}
+            width={40}
+            height={40}
+            alt="next-auth"
+          />
+        </Section>
+        <Heading style={h1}>Verify your email address</Heading>
+        <Text style={heroText}>
+          Your Verification code is below - enter it in your open browser window
+          and we&apos;ll help you get signed in.
+        </Text>
+
+        <Section style={codeBox}>
+          <Text style={VerificationCodeText}>{verificationCode}</Text>
+        </Section>
+
+        <Text style={text}>
+          If you didn&apos;t request this email, there&apos;s nothing to worry
+          about - you can safely ignore it.
+        </Text>
+      </Container>
+    </Body>
+  </Html>
+);
+
+export default VerifyEmail;
+
+const main = {
+  backgroundColor: "#ffffff",
+  margin: "0 auto",
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+};
+
+const container = {
+  maxWidth: "600px",
+  margin: "0 auto",
+};
+
+const logoContainer = {
+  marginTop: "32px",
+};
+
+const h1 = {
+  color: "#1d1c1d",
+  fontSize: "36px",
+  fontWeight: "700",
+  margin: "30px 0",
+  padding: "0",
+  lineHeight: "42px",
+};
+
+const heroText = {
+  fontSize: "20px",
+  lineHeight: "28px",
+  marginBottom: "30px",
+};
+
+const codeBox = {
+  background: "rgb(245, 244, 245)",
+  borderRadius: "4px",
+  marginRight: "50px",
+  marginBottom: "30px",
+  padding: "43px 23px",
+};
+
+const VerificationCodeText = {
+  fontSize: "30px",
+  textAlign: "center" as const,
+  verticalAlign: "middle",
+};
+
+const text = {
+  color: "#000",
+  fontSize: "14px",
+  lineHeight: "24px",
+};
