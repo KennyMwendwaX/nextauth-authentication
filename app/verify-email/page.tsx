70d19e1b8cf0a87a895c419d4aa1ca7fb29dbf2c
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-29 20:45:59 +0300

    Added redirect on no session on verify-email page

--- app/verify-email/page.tsx
+++ app/verify-email/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useSession } from "next-auth/react";
 import { redirect, useRouter, useSearchParams } from "next/navigation";
 import React, { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
@@ -27,6 +28,12 @@ export default function VerifyEmail() {
   const router = useRouter();
   const { control, handleSubmit } = useForm<FormData>();
   const [serverErrors, setServerErrors] = useState("");
+
+  const { data: session, status } = useSession();
+
+  if (session && status === "authenticated") {
+    redirect("/");
+  }
 
   const email = searchParams.get("email");
 
@@ -71,7 +78,7 @@ export default function VerifyEmail() {
       <div>
         <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-12">
           <div className="relative bg-white px-6 pt-10 pb-9 shadow-xl mx-auto w-full max-w-lg rounded-2xl">
-            <div className="mx-auto flex w-full max-w-md flex-col space-y-16">
+            <div className="mx-auto flex w-full max-w-md flex-col space-y-12">
               <div className="flex flex-col items-center justify-center text-center space-y-2">
                 <div className="font-semibold text-3xl">
                   <p>Email Verification</p>
